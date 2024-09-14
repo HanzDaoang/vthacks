56ea1591cb1d2abb8fd7ae43bee9fd4d04ba40e1
Dan Nguyen <danuyennguyen2412005@gmail.com>
2024-09-14 13:55:35 -0400

    additional changes

--- daddys-home/src/Pages/YourApp.jsx
+++ daddys-home/src/Pages/YourApp.jsx
@@ -9,42 +9,12 @@ const YourApp = withAuthInfo((props) => {
     const logoutFunction = useLogoutFunction()
     const { redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } = useRedirectFunctions()
 
-    // if (props.isLoggedIn) {
-    //     return (
-    //         <div>
-    //             <p>You are logged in as {props.user.email}</p>
-    //             <button onClick={() => redirectToAccountPage()}>Account</button>
-    //             <button onClick={() => logoutFunction(true)}>Logout</button>
-    //         </div>
-    //     )
-    // } else {
-    //     return (
-    //         <div>
-    //             <p>You are not logged in</p>
-    //             <button onClick={() => redirectToLoginPage()}>Login</button>
-    //             <button onClick={() => redirectToSignupPage()}>Signup</button>
-    //         </div>
-    //     )
-    // }
-
     return (
         <Box height="100vh" width="100vw" overflow="auto" sx={{ backgroundColor: '#ffffff' }} >
-
-        {/* Navigation Bar
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" sx={{ flexGrow: 1 }}>
-              My App
-            </Typography>
-            <Button color="inherit" component={Link} to="/">Home</Button>
+        <Box height="20vh" width="50vw">
+            <Button component={Link} to="/">Home</Button>
             <Button color="inherit" component={Link} to="/homes">Homes</Button>
-            <Button color="inherit" component={Link} to="/login">Login</Button>
-          </Toolbar>
-        </AppBar> */}
-        <Button component={Link} to="/">Home</Button>
-        <Button color="inherit" component={Link} to="/homes">Homes</Button>
-
-        {
+            {
             props.isLoggedIn ? (
                 <>
                     <p>Email: {props.user.email}</p>
@@ -52,19 +22,19 @@ const YourApp = withAuthInfo((props) => {
                 </>
             ) : (
                 <>
-                  <Button color="inherit" onClick={() => redirectToLoginPage()}>Login</Button>
-                  <Button color="inherit" onClick={() => redirectToSignupPage()}>Signup</Button>
+                    <Button color="inherit" onClick={() => redirectToLoginPage()}>Login</Button>
+                    <Button color="inherit" onClick={() => redirectToSignupPage()}>Signup</Button>
                 </>
             )
-        }
+        } 
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/homes" element={<Homes />} />
-          {/* <Route path="/auth" element={<Login />} /> */}
-        </Routes>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/homes" element={<Homes />} />
+            </Routes>
+        </Box>
   
-      </Box>
+        </Box>
     )
 })
 

91d7696b74c4d10791cedf9b19506f522e260625
arnavgolia <arnavgolia@gmail.com>
2024-09-14 22:27:29 +0000

    Sign up perks

---
+++ daddys-home/frontend/src/Components/Home/Perks.jsx
@@ -0,0 +1,49 @@
+import { Box, Typography } from "@mui/material";
+import desk from '../../assets/desk.svg';
+import safe from '../../assets/safe.svg';
+import thumbs from '../../assets/thumbs.svg';
+const Perks = () => {
+    return (
+        <Box display="flex" flexDirection="column"> 
+        <Box 
+        display="flex"
+        justifyContent="center"
+        alignItems="center">
+            
+            <Typography variant="h5" gutterBottom>
+                Join Daddy's Crib
+            </Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between" width="80vw">
+
+        
+        <Box display="flex" flexDirection="column"
+        justifyContent="center"
+        alignItems="center">
+        <img src={desk} width={300} height={300} />
+            <Typography variant="h6" mt={2}>
+                Post your experiences to Daddy
+            </Typography>
+        </Box>
+        <Box display="flex" flexDirection="column"
+        justifyContent="center"
+        alignItems="center">
+        <img src={safe} width={300} height={300} />
+            <Typography variant="h6" mt={2}>
+                Daddy keeps you safe
+            </Typography>
+        </Box>
+        <Box display="flex" flexDirection="column"
+        justifyContent="center"
+        alignItems="center">
+        <img src={thumbs} width={300} height={300} />
+            <Typography variant="h6" mt={2}>
+                Daddy's approval
+            </Typography>
+        </Box>
+        </Box>
+        </Box>
+
+    )
+}
+export default Perks;
